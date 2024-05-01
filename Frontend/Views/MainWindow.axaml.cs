a1c2d4da3c1a82ce065179262c14d88c6e191274
marco <furai93@gmail.com>
2024-05-01 19:09:37 +0200

    Added Avalonia (with a basic testing app)

---
+++ Frontend/Views/MainWindow.axaml.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Diagnostics;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace Frontend.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
